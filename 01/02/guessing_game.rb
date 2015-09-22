24c0eb8e4abf2ebf522a894e1f24a4360a1ee1de
James Dabbs <jamesdabbs@gmail.com>
2015-09-22 11:23:10 -0400

    Game is winnable

---
+++ 01/02/guessing_game.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Game
+  def initialize
+    @answer = rand(1 .. 10)
+  end
+
+  def over?
+    if @guessed_correct_answer
+      true
+    else
+      false
+    end
+  end
+
+  def check_guess guessed_number
+    if guessed_number == @answer
+      puts "Guessed correctly!"
+      @guessed_correct_answer = true
+    else
+      # ??
+    end
+  end
+end
+
+
+g = Game.new # starts game w/ rand # 1..50
+puts "Guess a random number between 1 and 50"
+
+until g.over?
+  puts "What is your guess?"
+  guess = gets.chomp.to_i
+  g.check_guess guess
+end
