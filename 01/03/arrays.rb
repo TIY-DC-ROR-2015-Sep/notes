1febce1371d8990bd4525bec760b2f478c6f8d3d
James Dabbs <jamesdabbs@gmail.com>
2015-09-23 09:41:24 -0400

    Add double function

--- 01/03/arrays.rb
+++ 01/03/arrays.rb
@@ -1,4 +1,7 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
 require 'pry'
 
 class Frankenclass
@@ -6,10 +9,19 @@ class Frankenclass
     sum = 0
     # numbers.each { |number| sum = sum + number }
     numbers.each do |number|
-      sum = sum + number
+      # sum = sum + number
+      sum += number
     end
     # return sum
     sum
+  end
+
+  def double(numbers)
+    result = []
+    numbers.each do |n|
+      result.push(n * 2)
+    end
+    result
   end
 end
 
@@ -17,5 +29,10 @@ class ArrayTests < Minitest::Test
   def test_can_add_up_numbers
     frank = Frankenclass.new
     assert_equal 20, frank.add([1,5,4,10])
+  end
+
+  def test_can_double_numbers
+    frank = Frankenclass.new
+    assert_equal [10,-4,14], frank.double([5,-2,7])
   end
 end
