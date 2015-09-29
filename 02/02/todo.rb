17044eae7c107eb6e85dc9922e44abd471dbfbf2
James Dabbs <jamesdabbs@gmail.com>
2015-09-29 11:20:11 -0400

    MVP

--- 02/02/todo.rb
+++ 02/02/todo.rb
@@ -1,12 +1,22 @@
-puts "What do you want to do today?"
-puts "1) Show list"
-puts "2) Add item"
+list = []
 
-selection = gets.chomp.to_i
-if selection == 1
-  # Do stuff to show list
-elsif selection == 2
-  # Add item
-else
-  # Say invalid
+loop do
+  puts "What do you want to do today?"
+  puts "1) Show list"
+  puts "2) Add item"
+
+  selection = gets.chomp.to_i
+  if selection == 1
+    # Do stuff to show list
+    list.each do |todo|
+      puts "* #{todo}"
+    end
+  elsif selection == 2
+    # Add item
+    puts "What do you need to do?"
+    item = gets.chomp
+    list.push item
+  else
+    # Say invalid
+  end
 end
