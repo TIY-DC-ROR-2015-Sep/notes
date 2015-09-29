14f591a8bf58872585d651848d0fff4f201c6d43
James Dabbs <jamesdabbs@gmail.com>
2015-09-29 12:04:29 -0400

    Persist to database as JSON

--- 02/02/todo.rb
+++ 02/02/todo.rb
@@ -1,8 +1,7 @@
-list = [
-  { description: "first item", done: false },
-  { description: "second item", done: true },
-  { description: "third item", done: false }
-]
+require 'pry'
+require 'json'
+
+list = JSON.parse File.read("database")
 
 loop do
   puts "What do you want to do today?"
@@ -15,33 +14,34 @@ loop do
   if selection == 1
     # Do stuff to show list
     list.each do |todo|
-      if todo[:done]
-        puts "X #{todo[:description]}"
+      if todo["done"]
+        puts "X #{todo['description']}"
       else
-        puts "- #{todo[:description]}"
+        puts "- #{todo['description']}"
       end
     end
   elsif selection == 2
     # Add item
     puts "What do you need to do?"
     description = gets.chomp
-    item = { description: description, done: false }
+    item = { "description" => description, "done" => false }
     list.push item
   elsif selection == 3
+    File.write "database", list.to_json
     puts "Have a nice day"
     break
   elsif selection == 4
     puts "What did you do?"
     number = 1
     list.each do |todo|
-      description = todo[:description]
+      description = todo["description"]
       puts "#{number}) #{description}"
       number = number + 1
     end
     selected_number = gets.chomp.to_i
     selected_todo = list[selected_number - 1]
-    puts "Good job doing #{selected_todo[:description]}!"
-    selected_todo[:done] = true
+    puts "Good job doing #{selected_todo['description']}!"
+    selected_todo["done"] = true
   else
     puts "I don't know what to do with #{selection}"
   end
