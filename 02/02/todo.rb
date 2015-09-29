3cf6f9de490db06effefca7a5101a4dfd0859ed4
James Dabbs <jamesdabbs@gmail.com>
2015-09-29 11:24:03 -0400

    Flesh out commands

--- 02/02/todo.rb
+++ 02/02/todo.rb
@@ -4,6 +4,7 @@ loop do
   puts "What do you want to do today?"
   puts "1) Show list"
   puts "2) Add item"
+  puts "3) Exit program"
 
   selection = gets.chomp.to_i
   if selection == 1
@@ -16,7 +17,12 @@ loop do
     puts "What do you need to do?"
     item = gets.chomp
     list.push item
+  elsif selection == 3
+    puts "Have a nice day"
+    break
   else
-    # Say invalid
+    puts "I don't know what to do with #{selection}"
   end
+
+  puts
 end
