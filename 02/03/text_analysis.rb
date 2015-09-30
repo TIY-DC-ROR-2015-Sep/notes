3120afdc820e57e1368d60f9a4914df10ee87145
James Dabbs <jamesdabbs@gmail.com>
2015-09-30 10:20:06 -0400

    Speed up multiple searches

--- 02/03/text_analysis.rb
+++ 02/03/text_analysis.rb
@@ -3,24 +3,28 @@ require 'pry'
 
 class Analyzer
   def initialize corpus
-    @corpus = corpus
+    @letters_array = []
+
+    @alphabet = ('a'..'z').to_a
+    26.times { @letters_array.push 0 }
+    corpus.each_char do |c|
+      i = @alphabet.index c
+      if i
+        @letters_array[i] += 1
+      end
+    end
   end
 
   def count letter
-    acc = 0
-    @corpus.each_char do |c|
-      if c == letter
-        acc += 1
-      end
-    end
-    acc
+    i = @alphabet.index letter
+    @letters_array[i]
   end
 end
 
 text = File.read "./words.txt"
 
 a = Analyzer.new text
-# binding.pry
+
 puts "There are #{a.count('e')} e's"
 puts "There are #{a.count('t')} t's"
 puts "There are #{a.count('z')} z's"
