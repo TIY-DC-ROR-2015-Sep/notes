afd49b14fe0585c6189f201647d0e2e168066e1a
James Dabbs <jamesdabbs@gmail.com>
2015-10-01 10:15:03 -0400

    More speed

--- 02/04/cosmic1.rb
+++ 02/04/cosmic1.rb
@@ -1,7 +1,24 @@
+require 'pry'
+
 words = File.read("words.txt").split("\n")
 
+words_by_length = {}
+words.each do |word|
+  # word ~ apple
+  # words_by_length[5].push "apple"
+
+  # words_by_length[word.length] ||= []
+  if words_by_length[word.length] == nil
+    words_by_length[word.length] = []
+  end
+
+  words_by_length[word.length].push word
+end
+
+
+
 def works? w1, w2
-  anagrams?(w1, w2) && chainable?(w1, w2)
+  w1 != w2 && anagrams?(w1, w2) && chainable?(w1, w2)
 end
 
 def anagrams? w1, w2
@@ -12,14 +29,24 @@ def chainable? w1, w2
   w1[-1] == w2[0]
 end
 
-counter = 0
-words.each do |fst|
-  words.each do |snd|
-    if works?(fst, snd)
-      counter += 1
-      puts [fst, snd]
+def find_pairs_from_list words
+  counter = 0
+  words.each do |fst|
+    words.each do |snd|
+      if works?(fst, snd)
+        counter += 1
+        puts [fst, snd]
+      end
     end
   end
+  counter
 end
 
-puts "There were #{counter} pairs that worked"
+sum = 0
+words_by_length.each do |word_length, word_list|
+  #puts "#{word_list.count} many words of length #{word_length}"
+  count = find_pairs_from_list word_list
+  sum += count
+end
+
+puts "There were #{sum} pairs that worked"
