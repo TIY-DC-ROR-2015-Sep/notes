e4ae3a49247103186cdff7a48b1186eaced52712
James Dabbs <jamesdabbs@gmail.com>
2015-10-05 11:55:25 -0400

    Pull out secret key

--- 03/01/apis.rb
+++ 03/01/apis.rb
@@ -1,18 +1,25 @@
 require "httparty"
 require "pry"
 
-response = HTTParty.get "https://api.github.com/repos/rails/rails/contributors"
-response.each_with_index do |contributor, i|
-  puts "#{i}) #{contributor["login"]}"
-end
+Token = File.read("./secret.txt").strip
 
-puts "Which number?"
-number = gets.chomp.to_i
+# response = HTTParty.get "https://api.github.com/repos/rails/rails/contributors"
+# response.each_with_index do |contributor, i|
+#   puts "#{i}) #{contributor["login"]}"
+# end
+#
+# puts "Which number?"
+# number = gets.chomp.to_i
+#
+# contributor_name = response[number]["login"]
+#
+#
+# response = HTTParty.get "https://api.github.com/users/#{contributor_name}"
+# puts "#{response["name"]} works for #{response["company"]} in #{response["location"]}"
 
-contributor_name = response[number]["login"]
-
-
-response = HTTParty.get "https://api.github.com/users/#{contributor_name}"
-puts "#{response["name"]} works for #{response["company"]} in #{response["location"]}"
-
+HTTParty.post "https://slack.com/api/chat.postMessage", body: {
+  channel: "#dc_sept2015_rails",
+  text: "Hello from apis.rb",
+  token: Token
+}
 # binding.pry
