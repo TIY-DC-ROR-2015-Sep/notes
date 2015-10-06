ada52bcf39d98c512608b72ab7da9904dbe2b92f
James Dabbs <jamesdabbs@gmail.com>
2015-10-06 10:00:04 -0400

    Make list of station objects

--- 03/02/station.rb
+++ 03/02/station.rb
@@ -1,2 +1,8 @@
 class Station
+  def initialize data_hash
+    @name = data_hash["Name"]
+  end
+
+  def upcoming_trains
+  end
 end
--- 03/02/transit.rb
+++ 03/02/transit.rb
@@ -1,3 +1,6 @@
+require "pry"
+require "httparty"
+
 require "./station"
 
 puts "Where are you?"
@@ -6,3 +9,15 @@ puts "Where are you?"
 
 lat  = 39
 long = -77
+
+r = HTTParty.get(
+  "https://api.wmata.com/Rail.svc/json/jStations",
+  headers: { "api_key" => "d311c928b8364eff80d7462f7938b2b1" }
+)
+
+stations = []
+r["Stations"].each do |h|
+  stations.push Station.new(h)
+end
+
+binding.pry
