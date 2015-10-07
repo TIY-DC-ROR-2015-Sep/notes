935ea0c80dd5627584a648eabb55a1e4ec8ec4de
James Dabbs <jamesdabbs@gmail.com>
2015-10-07 11:25:52 -0400

    MVP grocery list

--- 03/03/groceries.rb
+++ 03/03/groceries.rb
@@ -1,8 +1,30 @@
 require "sinatra/base"
+require "json"
 require "pry"
+
+List = ["apples", "bananas", "carrots"]
 
 class Groceries < Sinatra::Base
   set :logging, true
+
+  get "/groceries" do
+    List.to_json
+  end
+
+  post "/groceries" do
+    name = params[:name]
+
+    if name.nil?
+      status 400
+      body "You must provide a `name`"
+    elsif List.include? name
+      status 422
+      body "`#{name}` is already on the list"
+    else
+      List.push name
+      "OK, added '#{name}'"
+    end
+  end
 end
 
 Groceries.run!
