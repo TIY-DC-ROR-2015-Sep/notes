fad7d9692ff45b066c56973a9a4a50097dd42f24
James Dabbs <jamesdabbs@gmail.com>
2015-10-07 10:47:13 -0400

    Take data through header

--- 03/03/better_server.rb
+++ 03/03/better_server.rb
@@ -1,6 +1,16 @@
 require "sinatra/base"
 require "pry"
 
+class Dice
+  def initialize sides
+    @sides = sides.to_i
+  end
+
+  def roll
+    rand 1 .. @sides
+  end
+end
+
 class BetterServer < Sinatra::Base
   set :port, 8080
   set :logging, true
@@ -23,6 +33,17 @@ class BetterServer < Sinatra::Base
     else
       params[:phrase]
     end
+  end
+
+  delete "/roll" do
+    num = request.env["HTTP_X_NUM_DICE"] || 1
+    max = params[:sides] || 6
+
+    sum = 0
+    num.to_i.times do
+      sum += rand(1 .. max.to_i)
+    end
+    sum.to_s
   end
 end
 
